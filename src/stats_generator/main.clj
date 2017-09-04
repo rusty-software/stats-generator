b1e30e84111647de793bd35bb40bcf7c58250fa4
Rusty Bentley <mycoding.alert@gmail.com>
2017-09-04 11:57:41 -0500

    adds the rest of the builder functions and minimal testing

--- src/stats_generator/main.clj
+++ src/stats_generator/main.clj
@@ -3,8 +3,37 @@
 
 (def d6 [1 2 3 4 5 6])
 
-(defn ^:private roll-d6 []
+(defn roll-d6 [_]
   (rand-nth d6))
+
+(defn roll-4
+  "Rolls a d6 4 times"
+  []
+  (map roll-d6 (range 4)))
+
+(defn drop-lowest
+  "Given a vector, returns a vector sans its lowest value."
+  [v]
+  (rest (sort v)))
+
+(defn sum [v]
+  (apply + v))
+
+(defn roll-4-keep-3
+  "Rolls a 6-sided die 4 times, summing the highest 3 values.
+
+  Note that multiple arities exist, but the argument is ignored"
+  ([_]
+   (roll-4-keep-3))
+  ([]
+   (-> (roll-4)
+       (drop-lowest)
+       (sum))))
+
+(defn roll-stats
+  "Collects the results of 6 executions of rolling 4 dice and keeping the highest 3"
+  []
+  (map roll-4-keep-3 (range 6)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/stats_generator/main_test.clj
+++ test/stats_generator/main_test.clj
@@ -1,7 +1,32 @@
 (ns stats-generator.main-test
   (:require [clojure.test :refer :all]
-            [stats-generator.main :refer :all]))
+            [stats-generator.main :as main]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-roll-4
+  (let [roll (main/roll-4)]
+    (is (= 4 (count roll)))
+    (is (every? #(<= 1 % 6) roll))))
+
+(deftest test-drop-lowest
+  (testing "Given all different values in random order, drops the lowest"
+    (is (= [2 3 4] (main/drop-lowest [1 2 3 4])))
+    (is (= [2 3 4] (main/drop-lowest [4 2 3 1]))))
+  (testing "Given identical values, drops one of them"
+    (is (= [1 1 1] (main/drop-lowest [1 1 1 1]))))
+  (testing "Given some duplicated values, drops whatever's lowest"
+    (is (= [2 2 3] (main/drop-lowest [2 3 2 2])))
+    (is (= [5 6 6] (main/drop-lowest [5 6 4 6])))))
+
+(deftest test-sum
+  (is (= 18 (main/sum [6 6 6])))
+  (is (= 3 (main/sum [1 1 1])))
+  (is (= 12 (main/sum [3 4 5]))))
+
+(deftest test-roll-4-keep-3
+  (testing "Given a roll of 4 6-sided dice, sums the highest 3"
+    (is (<= 3 (main/roll-4-keep-3) 18))))
+
+(deftest test-roll-stats
+  (let [stats (main/roll-stats)]
+    (is (= 6 (count stats)))
+    (is (every? #(<= 3 % 18) stats))))
