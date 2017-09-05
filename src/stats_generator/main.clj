ac15b9930a906231c2b2b6ce92d22a47c6242fdc
Rusty Bentley <mycoding.alert@gmail.com>
2017-09-04 20:00:54 -0500

    adds range-3-to-18, refactors stats to multi-method

--- src/stats_generator/main.clj
+++ src/stats_generator/main.clj
@@ -1,10 +1,15 @@
 (ns stats-generator.main
-  (:gen-class))
+  (:gen-class)
+  (:import [java.util Date]))
 
 (def d6 [1 2 3 4 5 6])
+(def range-3-18 (range 3 19))
 
 (defn roll-d6 [_]
   (rand-nth d6))
+
+(defn roll-d3-18 [_]
+  (rand-nth range-3-18))
 
 (defn roll-4
   "Rolls a d6 4 times"
@@ -30,12 +35,40 @@
        (drop-lowest)
        (sum))))
 
-(defn roll-stats
-  "Collects the results of 6 executions of rolling 4 dice and keeping the highest 3"
-  []
+(defmulti roll-stats identity)
+
+(defmethod roll-stats :roll-4-keep-3
+  [_]
   (map roll-4-keep-3 (range 6)))
 
+(defmethod roll-stats :range-3-to-18
+  [_]
+  (map roll-d3-18 (range 6)))
+
+(defn now [] (new java.util.Date))
+
+(defn roll-all-18s [roll-type]
+ (let [counter (atom 0)]
+   (println "STARTED AT:" (now))
+    (loop [current-roll []]
+      (swap! counter inc)
+      (cond
+        (= 100000000 @counter)
+        (do
+          (println "Gave up after ONE HUNDRED MILLION sets...")
+          (println "ENDED AT:" (now)))
+
+        (= [18 18 18 18 18 18] current-roll)
+        (do
+          (println "All 18s took" @counter "attempt(s).")
+          (println "ENDED AT:" (now)))
+
+        :default
+        (do
+          (when (<= 5 (count (filter #(= 18 %) current-roll)))
+            (println "close call!" current-roll))
+          (recur (roll-stats roll-type)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (roll-all-18s :range-3-to-18))
