b236c6ec12aa562c54ade4f512aca1b4be28d85f
Rusty Bentley <mycoding.alert@gmail.com>
2018-10-29 18:17:58 -0500

    adds board init and queen collisions

--- src/stats_generator/queens.clj
+++ src/stats_generator/queens.clj
@@ -3,6 +3,41 @@
 (comment
   "The eight queens puzzle is the problem of placing eight chess queens on an 8×8 chessboard so that no two queens threaten each other. Thus, a solution requires that no two queens share the same row, column, or diagonal.")
 
+(defn init-board
+  "Returns a map of spaces and booleans for a square of n size."
+  [n]
+  (into {}
+        (for [r (range n)
+              c (range n)]
+          [[r c] false])))
+
+(defn queen-in-row?
+  "Given a row number and a board, returns true if a queen is in the row, otherwise false."
+  [row-num board]
+  (boolean (first (filter #(and (= row-num (first (key %)))
+                                (val %)) board))))
+
+(defn queen-in-column?
+  "Given a column number and a board, returns true if a queen is in the column, otherwise false."
+  [col-num board]
+  (boolean (first (filter #(and (= col-num (second (key %)))
+                                (val %)) board))))
+
+(defn abs [n]
+  (if (neg? n) (* -1 n) n))
+
+(defn queen-in-diagonal?
+  "Given a space and a board, returns true if a queen is in a diagonal, otherwise false."
+  [[row column] board]
+  (boolean (first (filter #(and (= (abs (- row (first (key %))))
+                                   (abs (- column (second (key %)))))
+                                (val %)) board)))
+  #_(let [board-size (inc (apply max (map first (keys board))))
+        diagonal-rows (concat (range row) (range row board-size))
+        diagonal-columns (concat (range column) (range column board-size))]
+    (println diagonal-rows)
+    (println diagonal-columns)))
+
 (defn solve
   "Produces a solution to the queens puzzle given a number of queens. Board size will be the same dimension as number of queens."
   [n])
--- test/stats_generator/queens_test.clj
+++ test/stats_generator/queens_test.clj
@@ -1,2 +1,32 @@
 (ns stats-generator.queens-test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is]]
+            [stats-generator.queens :as q]))
+
+(deftest test-init-board
+  (is (= {[0 0] false [0 1] false [0 2] false
+          [1 0] false [1 1] false [1 2] false
+          [2 0] false [2 1] false [2 2] false}
+         (q/init-board 3))))
+
+(deftest test-queen-in-row?
+  (let [board {[0 0] false [0 1] false [0 2] false
+               [1 0] false [1 1] false [1 2] true
+               [2 0] false [2 1] false [2 2] false}]
+    (is (not (q/queen-in-row? 0 board)))
+    (is (q/queen-in-row? 1 board))))
+
+(deftest test-queen-in-column?
+  (let [board {[0 0] false [0 1] false [0 2] false
+               [1 0] false [1 1] false [1 2] true
+               [2 0] false [2 1] false [2 2] false}]
+    (is (not (q/queen-in-column? 1 board)))
+    (is (q/queen-in-column? 2 board))))
+
+(deftest test-queen-in-diagonal?
+  (let [board {[0 0] false [0 1] false [0 2] false
+               [1 0] true [1 1] false [1 2] false
+               [2 0] false [2 1] false [2 2] false}]
+    (is (not (q/queen-in-diagonal? [0 2] board)))
+    (is (q/queen-in-diagonal? [0 1] board))
+    (is (q/queen-in-diagonal? [2 1] board))))
+
