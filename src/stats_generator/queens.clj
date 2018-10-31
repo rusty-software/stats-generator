5a1e7a3b75e9736db72db2e4e3f3a770d08aa1be
Rusty Bentley <mycoding.alert@gmail.com>
2018-10-30 20:22:51 -0500

    working on final recursion -- still bad at it. ;)

--- src/stats_generator/queens.clj
+++ src/stats_generator/queens.clj
@@ -11,17 +11,29 @@
               c (range n)]
           [[r c] false])))
 
+(defn queen-space
+  "Given a board and row, returns the column in which a queen resides, -1 if no queen is found."
+  [board row-num]
+  (let [space (filter #(and (= row-num (first (key %)))
+                            (val %))
+                      board)]
+    (if (seq space)
+      (key (first space))
+      nil)))
+
 (defn queen-in-row?
   "Given a row number and a board, returns true if a queen is in the row, otherwise false."
   [row-num board]
   (boolean (first (filter #(and (= row-num (first (key %)))
-                                (val %)) board))))
+                                (val %))
+                          board))))
 
 (defn queen-in-column?
   "Given a column number and a board, returns true if a queen is in the column, otherwise false."
   [col-num board]
   (boolean (first (filter #(and (= col-num (second (key %)))
-                                (val %)) board))))
+                                (val %))
+                          board))))
 
 (defn abs [n]
   (if (neg? n) (* -1 n) n))
@@ -31,16 +43,73 @@
   [[row column] board]
   (boolean (first (filter #(and (= (abs (- row (first (key %))))
                                    (abs (- column (second (key %)))))
-                                (val %)) board)))
-  #_(let [board-size (inc (apply max (map first (keys board))))
-        diagonal-rows (concat (range row) (range row board-size))
-        diagonal-columns (concat (range column) (range column board-size))]
-    (println diagonal-rows)
-    (println diagonal-columns)))
+                                (val %))
+                          board))))
+
+(defn queen-at-end-of-first-row?
+  "Given a board, returns true if the queen in row 0 in in the space furthest right."
+  [board board-size]
+  (boolean (first (filter #(and (zero? (first (key %)))
+                                (= (dec board-size) (second (key %)))
+                                (val %))
+                          board))))
+
+
+
+(defn place-next-queen
+  "Given a board, size, and a queen number to place, returns a placement result with an indicator of whether or not the queen was successfully placed and an updated board.
+
+  Uses the fact that only one queen per row is allowed, and only attempts insertion into that row."
+  ([board board-size queen-to-place]
+    (place-next-queen board board-size queen-to-place false))
+  ([board board-size queen-to-place move-queen-right?]
+   (loop [{:keys [board column] :as state} {:board board :column 0}]
+     (let [queen-space-this-row (queen-space board queen-to-place)
+           column (if (and move-queen-right? queen-space-this-row)
+                    (inc (second queen-space-this-row))
+                    column)
+           space [queen-to-place column]]
+       (cond
+         (= board-size column)
+         (assoc state :queen-placed? false)
+
+         (not (or (queen-in-column? column board)
+                  (queen-in-diagonal? space board)))
+         (-> state
+             (assoc-in [:board space] true)
+             (assoc :queen-placed? true))
+
+         :else
+         (recur (update state :column inc)))))))
 
 (defn solve
   "Produces a solution to the queens puzzle given a number of queens. Board size will be the same dimension as number of queens."
-  [n])
+  [n]
+  #_(let [board (init-board n)
+        initial-state {:board board
+                       :queen-to-place 0
+                       :queen-placed? false}]
+    (loop [{:keys [board queen-to-place queen-placed?] :as state} initial-state]
+      (println board)
+      (cond
+        (= n queen-to-place)
+        {:result :solved :board board}
+
+        (and (not queen-placed?)
+             (queen-at-end-of-first-row? board n))
+        {:result :unsolvable :board board}
+
+        (not queen-placed?)
+        (let [placement-result (place-next-queen board n (dec queen-to-place) true)])
+
+        :else
+        (let [placement-result (place-next-queen board n queen-to-place)]
+          (if (:queen-placed? placement-result)
+            (recur (assoc state :queen-placed? true
+                                :queen-to-place (inc queen-to-place)
+                                :board (:board placement-result)))
+            (recur (assoc state :queen-placed? false))))
+        ))))
 
 (defn init []
   (solve 8))
--- test/stats_generator/queens_test.clj
+++ test/stats_generator/queens_test.clj
@@ -1,5 +1,5 @@
 (ns stats-generator.queens-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             [stats-generator.queens :as q]))
 
 (deftest test-init-board
@@ -7,6 +7,14 @@
           [1 0] false [1 1] false [1 2] false
           [2 0] false [2 1] false [2 2] false}
          (q/init-board 3))))
+
+(deftest test-queen-space
+  (let [board {[0 0] true [0 1] false [0 2] false
+               [1 0] false [1 1] false [1 2] true
+               [2 0] false [2 1] false [2 2] false}]
+    (is (= [0 0] (q/queen-space board 0)))
+    (is (= [1 2] (q/queen-space board 1)))
+    (is (nil? (q/queen-space board 2)))))
 
 (deftest test-queen-in-row?
   (let [board {[0 0] false [0 1] false [0 2] false
@@ -30,3 +38,57 @@
     (is (q/queen-in-diagonal? [0 1] board))
     (is (q/queen-in-diagonal? [2 1] board))))
 
+(deftest test-queen-at-end-of-first-row?
+  (is (q/queen-at-end-of-first-row? {[0 0] false [0 1] false [0 2] true
+                                     [1 0] true [1 1] false [1 2] false
+                                     [2 0] false [2 1] false [2 2] false}
+                                    3))
+  (is (not (q/queen-at-end-of-first-row? {[0 0] false [0 1] false [0 2] false
+                                          [1 0] true [1 1] false [1 2] false
+                                          [2 0] false [2 1] false [2 2] false}
+                                         3))))
+
+(deftest test-place-next-queen
+  (testing "queen is placeable"
+    (let [board {[0 0] true [0 1] false [0 2] false
+                 [1 0] false [1 1] false [1 2] false
+                 [2 0] false [2 1] false [2 2] false}
+          {:keys [queen-placed? board]} (q/place-next-queen board 3 1)]
+      (is queen-placed?)
+      (is (= {[0 0] true [0 1] false [0 2] false
+              [1 0] false [1 1] false [1 2] true
+              [2 0] false [2 1] false [2 2] false}
+             board))))
+  (testing "queen is not placeable"
+    (let [board {[0 0] true [0 1] false [0 2] false
+                 [1 0] false [1 1] false [1 2] true
+                 [2 0] false [2 1] false [2 2] false}
+          {:keys [queen-placed? board]} (q/place-next-queen board 3 2)]
+      (is (not queen-placed?))
+      (is (= {[0 0] true [0 1] false [0 2] false
+              [1 0] false [1 1] false [1 2] true
+              [2 0] false [2 1] false [2 2] false}
+             board))))
+  (testing "queen needs to move"
+    (let [board {[0 0] true [0 1] false [0 2] false [0 3] false
+                 [1 0] false [1 1] false [1 2] true [1 3] false
+                 [2 0] false [2 1] false [2 2] false [2 3] false
+                 [3 0] false [3 1] false [3 2] false [3 3] false }
+          {:keys [queen-placed? board]} (q/place-next-queen board 4 1 true)]
+      (is (not queen-placed?))
+      (is (= {[0 0] true [0 1] false [0 2] false
+              [1 0] false [1 1] false [1 2] true
+              [2 0] false [2 1] false [2 2] false}
+             board)))))
+
+#_(deftest test-solve
+  (testing "board is unsolvable"
+    (let [state (q/solve 3)]
+      (println state))
+    #_(let [{:keys [queen-placed? board]} (q/place-next-queen board 3 2)]
+      (is (not queen-placed?))
+      (is (= {[0 0] true [0 1] false [0 2] false
+              [1 0] false [1 1] false [1 2] true
+              [2 0] false [2 1] false [2 2] false}
+             board))))
+  )
